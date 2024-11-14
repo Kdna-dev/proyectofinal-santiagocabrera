07f7d70268b6857e46ac76df717879fb78743075
kdna-dev <kdna.dev@gmail.com>
2024-11-13 23:10:32 -0300

    Cambios agregados en el commit: - Dependencia para utilizacion de Aspect Oriented Programming - Configuracion para habilitar AOP en la aplicacion - Servicio de loggin programado en AOP - Configuracion de estrategia de loggin mediante RollingPolicy - Validacion de existencia de carpeta logs PostConstruct - Ignore log files

--- src/main/java/com/kdnadev/proyectofinal_santiagocabrera/ProyectofinalSantiagocabreraApplication.java
+++ src/main/java/com/kdnadev/proyectofinal_santiagocabrera/ProyectofinalSantiagocabreraApplication.java
@@ -3,8 +3,21 @@ package com.kdnadev.proyectofinal_santiagocabrera;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
+import java.io.File;
+
 @SpringBootApplication
 public class ProyectofinalSantiagocabreraApplication {
+
+	@PostConstruct
+	public void init() {
+		// Crear directorio de logs si no existe
+		File logDir = new File("logs/archived");
+		if (!logDir.exists()) {
+			logDir.mkdirs();
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectofinalSantiagocabreraApplication.class, args);
